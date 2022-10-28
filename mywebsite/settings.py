1d1fb6668157efe8332939dd0f4b53313dc0c41e
Badenla <baderinwadamola@gmail.com>
2022-10-28 14:53:25 +0100

    settings

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -51,13 +51,13 @@ CORS_ALLOW_METHODS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
