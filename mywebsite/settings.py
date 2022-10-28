f8acddb3ac4869b4ef6f4479ed72788e593a4e46
Badenla <baderinwadamola@gmail.com>
2022-10-28 22:58:46 +0100

    settings

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -42,11 +42,8 @@ INSTALLED_APPS = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000/',
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "https://cors-test.codehappy.dev/"
+    'https://cors-test.codehappy.dev',
+    'http://127.0.0.1:8000',
 ]
 
 default_app_config = "myapp.apps.MyAppConfig"
