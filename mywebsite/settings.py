72b519b26d3433e872686733049256a72ccdfc6f
Badenla <baderinwadamola@gmail.com>
2022-10-28 15:27:41 +0100

    settings

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
 ]
 
 CSRF_TRUSTED_ORIGINS = ['https://https://cors-test.codehappy.dev/']
